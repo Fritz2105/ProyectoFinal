2cd0e1bdefc8a525527691c0b92947e2a5cf7805
Fritz2105 <al204686@alumnos.uacj.mx>
2023-05-15 14:05:19 -0600

    Primer Commit

---
+++ php/pagina3.php
@@ -0,0 +1,189 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Encuesta</title>
+    <link rel="shortcut icon" href="./img/faviconImg.png" type="image/x-icon">
+    <link rel="stylesheet" href="./css/normalize.css">
+    <link rel="stylesheet" href="./css/estilos.css">
+
+    <meta name="theme-color" content="#2091F9">
+
+</head>
+
+<body>
+
+    <header class="banner_3">
+        <nav class="nav container">
+            <div class="nav__logo">
+                <h2 class="nav__title">Dejanos conocerte</h2>
+            </div>
+
+            <ul class="nav__link nav__link--menu">
+                <li class="nav__items">
+                    <a href="index.html" class="nav__links">Inicio</a>
+                </li>
+                <li class="nav__items">
+                    <a href="pagina2.html" class="nav__links">Todo sobre</a>
+                </li>
+                <li class="nav__items">
+                    <a href="pagina3.html" class="nav__links">Dejanos conocerte</a>
+                </li>
+                <li class="nav__items">
+                    <a href="pagina4.html" class="nav__links">Voces</a>
+                </li>
+
+            </ul>
+
+        </nav>
+
+        <section class="banner__container container">
+
+            <h1 class="banner__title">_encuesta</h1>
+            <p class="banner__paragraph">Toma una pequeña y entretenida _encuesta para que nos permitas conocer tu opinión mejor. Nos interesa saber de que manera impacta esta información en tu manera de pensar. <br> (∩_∩)
+            </p>
+            <a href="#formulario" class="cta">Responder</a>
+        
+        </section>
+    </header>
+
+    <main>
+        <section class="container about">
+            <a name="formulario"> <h2 class="subtitle">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, dolore!</h2> </a>
+            <p class="about__paragraph">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam ea ut magnam doloribus impedit, quod reprehenderit dolor ad itaque quis.</p>
+
+           
+            <section class="footer__container container">
+                <form class="footer__form" action="https://formspree.io/f/mknkkrkj" method="POST">
+                   
+                    <div class="footer__inputs">
+                        <input type="text" placeholder="Nombre:" class="footer__input" name="_username" >
+                        
+                    </div>
+                  
+                    <div class="footer__inputs">
+                        <input type="email" placeholder="Email:" class="footer__input" name="_replyto">
+                        <input type="submit" value="Registrate" class="footer__submit">
+                    </div>
+
+                    <div class="footer__inputs">
+                    <br><br><li>
+                        Piensas que el Arte se pierde cuando usas la Tecnología en el?: <br><br>
+                            <select class="footer__input" name="_encuesta">
+                                <option value="1">.......................................................................................</option>
+                                <option value="Si">Pienso que sí!</option>
+                                <option value="Neutral">Mmm...no lo sé</option>
+                                <option value="No">Claro que no!</option>
+                            </select>
+                    </li>
+                    </div>
+
+                    <div class="footer__inputs">
+                        <br><br><li>
+                            Cómo te parece la codificación de voces humanas para sintetizarlas en un software?: <br><br>
+                                <select class="footer__input" name="_encuesta">
+                                    <option value="1">.......................................................................................</option>
+                                    <option value="Si">Es ciertamente asombroso</option>
+                                    <option value="Neutral">No me parece la gran cosa</option>
+                                    <option value="No">La verdad me parece algo aterrador</option>
+                                </select>
+                        </li>
+                        </div>
+
+                        <div class="footer__inputs">
+                            <br><br><li>
+                                Si tuvieras la oportunidad ¿Prestarías tu voz para un personaje?: <br><br>
+                                    <select class="footer__input" name="_encuesta">
+                                        <option value="1">.......................................................................................</option>
+                                        <option value="Si">Sí, que emocionante!</option>
+                                        <option value="Neutral">¿No sería un poco raro?</option>
+                                        <option value="No">Mmm...No estoy comodx con eso</option>
+                                    </select>
+                            </li>
+                            </div>
+                            
+                     <div class="footer__inputs">
+                            <br><br><li>
+                                La idea de tener un Idol esternamente joven es...: <br><br>
+                                    <select class="footer__input" name="_encuesta">
+                                        <option value="1">.......................................................................................</option>
+                                        <option value="Si">Bastante conveniente para lucrar</option>
+                                        <option value="Neutral">...Raro...</option>
+                                        <option value="No">Es practico</option>
+                                    </select>
+                            </li>
+                            </div>
+
+                     <div class="footer__inputs">
+                            <br><br><li>
+                                Sabías que un hombre contrajo matrimonio con Hatsune Miku(La primera voz)?: <br><br>
+                                    <select class="footer__input" name="_encuesta">
+                                        <option value="1">.......................................................................................</option>
+                                        <option value="Si">QUE QUE?!?!!(╬☉д⊙)⊰⊹ฺ
+                                        </option>
+                                        <option value="Neutral">Si, vi la notica en google (⊙︿⊙)
+                                        </option>
+                                        <option value="No">PERO COMO ◉ ͟ʖ ◉ 
+                                        </option>
+                                    </select>
+                            </li>
+                     </div>
+                     <div class="footer__inputs">
+                        <input type="submit" value="Enviar respuestas" class="footer__submit">
+                    </div>
+
+                </form>
+               
+            </section>
+
+            <br><div class="knowledege__texts">
+                <a href="pagina3.html" class="cta">Página Anterior</a> <br><br>
+                <a href="pagina4.html" class="cta">Página Siguiente</a>
+            </div>
+                
+        </section>       
+
+    <footer class="footer">
+        <section class="footer__container container">
+            <nav class="nav nav--footer">
+                <h2 class="footer__title">Dejanos conocerte</h2>
+
+                <ul class="nav__link nav__link--footer">
+                    <li class="nav__items">
+                        <a href="index.html" class="nav__links">Inicio</a>
+                    </li>
+                    <li class="nav__items">
+                        <a href="pagina2.html" class="nav__links">Todo sobre</a>
+                    </li>
+                    <li class="nav__items">
+                        <a href="pagina3.html" class="nav__links">Dejanos conocerte</a>
+                    </li>
+                    <li class="nav__items">
+                        <a href="pagina4.html" class="nav__links">Voces</a>
+                    </li>
+                </ul>
+            </nav>
+
+        </section>
+
+        <section class="footer__copy container">
+            <div class="footer__social">
+                <a href="https://www.facebook.com/somosuacj/" class="footer__icons"><img src="./img/facebook.svg" class="footer__img"></a>
+                <a href="https://twitter.com/uacjoficial?lang=en" class="footer__icons"><img src="./img/twitter.svg" class="footer__img"></a>
+            </div>
+
+            <h3 class="footer__copyright">（｡◑ヮ◑｡）II Frida Balcázar II 204686 II （｡◑ヮ◑｡）
+</h3>
+        </section>
+    </footer>
+
+    <?php 
+        include("registrar.php");
+        ?>
+
+</body>
+
+</html>
